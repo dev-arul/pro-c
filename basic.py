30212b0ad03c0630995969043647ad00a897d939
dev-arul <arularul83@gmail.com>
2021-04-01 15:01:44 +0530

    pro-c

---
+++ basic.py
@@ -0,0 +1,10 @@
+x='Dane'
+y='Rechal'
+z='Sam'
+print('My name is {1} and {0} is my wife and {2} is my son'.format(y,x,z))
+print('My name is {b} and {a} is my wife and {c} is my son'.format(a=y,b=x,c=z))
+
+a = (1,2,3,4,5,6,7,8,9)
+print(type(a))
+print(a.count(3))
+print(a.index(5))
