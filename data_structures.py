12be0ac1aaad69d35f9216402db8b2bca75523d1
dev-arul <arularul83@gmail.com>
2021-04-02 10:29:57 +0530

    pro-c

---
+++ data_structures.py
@@ -0,0 +1,24 @@
+'''
+#Data structures
+#Data types
+    #int
+    #str
+    #float
+    #boolen
+
+#collection datatype
+    #List
+    #tuple (Immutable)
+    #dictionary
+    #set
+        #frozon set (Immutable)
+#None
+#Range
+'''
+
+#Data types
+a = 2
+b = 'str'
+c = 10.3
+d = True
+print(type(a),type(b),type(c),type(d))
