c9d46f441a5f876ffc74e4f5e41f6063864a8ed5
dev-arul <arularul83@gmail.com>
2021-04-02 10:41:44 +0530

    pro-c

--- data_structures.py
+++ data_structures.py
@@ -22,3 +22,12 @@ b = 'str'
 c = 10.3
 d = True
 print(type(a),type(b),type(c),type(d))
+
+#List
+
+x = [2, 4, 5, 6, 7, 8, 9]
+print(x)
+x.insert(1,3)
+print(x)
+print(x.index(5))
+print(x.count(5))
